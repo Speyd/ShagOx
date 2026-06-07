1735f39e0f1608007c3aca882505edfc62b87e7f
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-07 12:38:17 +0200

    chore: configure database connection

--- backend/SchagoxServer/SchagoxServer/Api/Program.cs
+++ backend/SchagoxServer/SchagoxServer/Api/Program.cs
@@ -1,14 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using SchagoxServer.Api.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddDatabase(builder.Configuration);
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();

2e1eeba9b0a17a2690efac0077dc2f9cbe934627
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-07 21:22:44 +0200

    chore: Rename solution and assemblies backend

--- backend/ShagOxServer/ShagOxServer.Api/Program.cs
+++ backend/ShagOxServer/ShagOxServer.Api/Program.cs
@@ -3,17 +3,14 @@ using SchagoxServer.Api.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDatabase(builder.Configuration);
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 var app = builder.Build();
 
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }

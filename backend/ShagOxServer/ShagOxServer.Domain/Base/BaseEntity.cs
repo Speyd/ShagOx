75f4f6920b62cacef50c5a423360683b7712985a
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-07 21:29:08 +0200

    feat(location): add Region entity

---
+++ backend/ShagOxServer/ShagOxServer.Domain/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShagOxServer.Domain.Base;
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+
+    public abstract override string ToString();
+}
---
+++ backend/ShagOxServer/ShagOxServer.Domain/Entities/Region.cs
@@ -0,0 +1,16 @@
+﻿using ShagOxServer.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShagOxServer.Domain.Entities;
+
+public class Region : BaseEntity
+{
+    public string Name { get; set; } = "";
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}

f4b933ce5bb95fbea40100cc54bc80821f228e3b
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-08 20:17:32 +0200

    feat(user): link User with Role

--- backend/ShagOxServer/ShagOxServer.Domain/Entities/Account/Role.cs
+++ backend/ShagOxServer/ShagOxServer.Domain/Entities/Account/Role.cs
@@ -7,6 +7,8 @@ public class Role : BaseEntity
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
 
+    public List<UserRole> UserRoles { get; set; } = new List<UserRole>();
+
     public override string ToString()
     {
         return Name;

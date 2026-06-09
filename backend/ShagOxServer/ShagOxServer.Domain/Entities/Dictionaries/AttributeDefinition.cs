33a85240f83b046d99a292c2c73b484c9dd20f7d
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-09 20:38:18 +0200

    feat(dict): add AttributeDefinition entity

---
+++ backend/ShagOxServer/ShagOxServer.Domain/Entities/Dictionaries/AttributeDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShagOxServer.Domain.Entities.Dictionaries;
+public class AttributeDefinition
+{
+    public int Id { get; set; }
+
+    public string Key { get; set; } = null!;
+    public string Type { get; set; } = null!;
+    public bool Required { get; set; }
+
+    public int? Min { get; set; }
+    public int? Max { get; set; }
+}

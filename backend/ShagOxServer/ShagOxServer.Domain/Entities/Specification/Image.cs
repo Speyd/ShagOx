55af2f16858878ef61667f632a28e1204df59016
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-09 21:32:08 +0200

    feat(media): add Image entity

---
+++ backend/ShagOxServer/ShagOxServer.Domain/Entities/Specification/Image.cs
@@ -0,0 +1,13 @@
+﻿using ShagOxServer.Domain.Base;
+
+namespace ShagOxServer.Domain.Entities.Specification;
+public class Image : BaseEntity
+{
+    public string Url { get; set; } = "";
+    public int Order { get; set; }
+
+    public override string ToString()
+    {
+        return $"Order: {Order} | Url: {Url}";
+    }
+}

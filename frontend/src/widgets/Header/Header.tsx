9180abe9237058b303bb7dafdb3f5f5b4301842c
Egor Starostenko <egorstarostenko345@gmail.com>
2026-06-17 22:24:15 +0300

    update navigation

--- frontend/src/widgets/Header/Header.tsx
+++ frontend/src/widgets/Header/Header.tsx
@@ -1,19 +1,20 @@
 import Container from "@/shared/ui/Container";
 import styles from "./Header.module.css";
 import Button from "@/shared/ui/Button";
-import Input from "@/shared/ui/Input";
-import PageTitle from "@/shared/ui/PageTitle";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+
   return (
     <header className={styles.header}>
       <Container>
         <div className={styles.content}>
-          <PageTitle>Header</PageTitle>
+          <Link to="/" className={styles.logo}>
+            ShagOx
+          </Link>
 
-          <Input placeholder="Email" />
-
-          <Button>Login</Button>
+          <Button onClick={() => navigate("/login")}>Login</Button>
         </div>
       </Container>
     </header>

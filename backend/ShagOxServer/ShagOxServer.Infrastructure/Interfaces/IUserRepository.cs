bd8f2c98177bcacf14e554948b74ae4937867082
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-17 21:30:25 +0200

    feat(repo-user): add UserRepository

---
+++ backend/ShagOxServer/ShagOxServer.Infrastructure/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShagOxServer.Domain.Entities.Account;
+
+namespace ShagOxServer.Infrastructure.Interfaces;
+public interface IUserRepository
+{
+    Task<User?> GetByEmailAsync(string email);
+
+    Task<User?> GetByIdAsync(int id);
+
+    Task AddAsync(User user);
+
+    Task<bool> ExistsAsync(string email);
+}
---
+++ backend/ShagOxServer/ShagOxServer.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShagOxServer.Domain.Entities.Account;
+using ShagOxServer.Infrastructure.Interfaces;
+
+namespace ShagOxServer.Infrastructure.Persistence.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AppDbContext _db;
+    public UserRepository(AppDbContext db)
+    {
+        _db = db;
+    }
+    public async Task AddAsync(User user)
+    {
+        await _db.Users.AddAsync(user);
+
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task<bool> ExistsAsync(string email)
+    {
+        return await _db.Users
+            .AnyAsync(x => x.Email == email);
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _db.Users.FirstOrDefaultAsync(x => x.Email == email);
+    }
+
+    public async Task<User?> GetByIdAsync(int id)
+    {
+        return await _db.Users.FirstOrDefaultAsync(x => x.Id == id);
+    }
+}

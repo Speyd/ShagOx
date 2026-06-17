a575c61031f19241e051c5cf0e530432e3b8a8f0
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-17 22:47:36 +0200

    chore(db): User entity changed

--- backend/ShagOxServer/ShagOxServer.Api/Program.cs
+++ backend/ShagOxServer/ShagOxServer.Api/Program.cs
@@ -1,13 +1,36 @@
 using Microsoft.AspNetCore.Identity;
 using SchagoxServer.Api.DependencyInjection;
+using ShagOxServer.Application.Interfaces;
+using ShagOxServer.Application.Services;
+using ShagOxServer.Application.Validators;
 using ShagOxServer.Domain.Entities.Account;
+using ShagOxServer.Infrastructure.Interfaces;
+using ShagOxServer.Infrastructure.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDatabase(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddScoped<
+    IAuthService,
+    AuthService>();
+
+
+builder.Services.AddScoped<
+    IUserRepository,
+    UserRepository>();
+
+
+builder.Services.AddScoped<
+    IPasswordHasher<User>,
+    PasswordHasher<User>>();
+
+
+builder.Services.AddScoped<
+    IContactValidator,
+    ContactValidator>();
+
 
 var app = builder.Build();
 
--- backend/ShagOxServer/ShagOxServer.Domain/Entities/Account/User.cs
+++ backend/ShagOxServer/ShagOxServer.Domain/Entities/Account/User.cs
@@ -4,21 +4,21 @@ using ShagOxServer.Domain.Entities.Location;
 namespace ShagOxServer.Domain.Entities.Account;
 public class User : BaseEntity
 {
-    public string Surname { get; set; } = "";
-    public string Name { get; set; } = "";
+    public string? Surname { get; set; }
+    public string? Name { get; set; }
 
     public string PasswordHash { get; set; } = "";
 
-    public string Phone { get; set; } = "";
-    public string Email { get; set; } = "";
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
 
-    public string Avatar { get; set; } = "";
+    public string? Avatar { get; set; }
 
-    public int CityId { get; set; }
+    public int? CityId { get; set; }
     public City? City { get; set; } = null;
 
     public DateTime? LastSeenAt { get; set; }
-    public DateTime RegisteredAt { get; set; }
+    public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
 
     public List<UserRole> UserRoles { get; set; } = new List<UserRole>();
 

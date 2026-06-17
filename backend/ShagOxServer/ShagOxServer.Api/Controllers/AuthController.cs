984bb8ff1c7494e05b74e3f3e34ddb9e7c407607
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-17 22:24:07 +0200

    feat(controll): add AuthController

---
+++ backend/ShagOxServer/ShagOxServer.Api/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShagOxServer.Application.DTOs.Auth;
+using ShagOxServer.Application.Interfaces;
+
+namespace ShagOxServer.Api.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+
+    public AuthController(
+        IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+
+    [HttpPost("register")]
+    public async Task<ActionResult<RegisterResponse>> Register(
+        RegisterRequest request)
+    {
+        var result =
+            await _authService.RegisterAsync(request);
+
+        return Ok(result);
+    }
+}

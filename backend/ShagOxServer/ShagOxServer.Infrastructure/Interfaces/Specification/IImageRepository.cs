d2dc749240359467c59f05eebdb8e354a3d31f29
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-25 00:00:16 +0200

    feat(image): add ImageRepository

---
+++ backend/ShagOxServer/ShagOxServer.Infrastructure/Interfaces/Specification/IImageRepository.cs
@@ -0,0 +1,11 @@
+﻿using ShagOxServer.Domain.Entities.Specification;
+
+namespace ShagOxServer.Infrastructure.Interfaces.Specification;
+public interface IImageRepository
+{
+    Task<Image?> GetByIdAsync(int id);
+
+    Task AddAsync(Image image);
+
+    Task<bool> UpdateAsync(Image image);
+}

ae3c800f24e3fd92db7444ae688836ffdea34c9c
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-25 10:46:58 +0200

    feat(advert): add AdvertisementUpdateResponse

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Advertisements/Update/AdvertisementUpdateResponse.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ShagOxServer.Application.DTOs.Advertisements.Update;
+public sealed record AdvertisementUpdateResponse 
+(
+    string? Title = null,
+    string? Description = null,
+    int? Price = null,
+    int? CurrencyId = null,
+    int? CategoryId = null,
+    Dictionary<string, string>? Property = null,
+    int? Popularity = null
+);

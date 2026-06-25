b5dca682cee246e0bb57dcb1d57abd0c37ca30b1
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-25 10:51:15 +0200

    feat(advert): add IAdvertisementUpdateService and AdvertisementUpdateRequest

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Advertisements/Update/AdvertisementUpdateRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace ShagOxServer.Application.DTOs.Advertisements.Update;
+public sealed record AdvertisementUpdateRequest
+(
+    string? Title = null,
+    string? Description = null,
+    int? Price = null,
+    int? CurrencyId = null,
+    int? CategoryId = null,
+    Dictionary<string, string>? Property = null,
+    int? Popularity = null
+);
--- backend/ShagOxServer/ShagOxServer.Application/DTOs/Advertisements/Update/AdvertisementUpdateResponse.cs
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Advertisements/Update/AdvertisementUpdateResponse.cs
@@ -1,12 +1,8 @@
-﻿
+﻿using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
 namespace ShagOxServer.Application.DTOs.Advertisements.Update;
 public sealed record AdvertisementUpdateResponse 
 (
-    string? Title = null,
-    string? Description = null,
-    int? Price = null,
-    int? CurrencyId = null,
-    int? CategoryId = null,
-    Dictionary<string, string>? Property = null,
-    int? Popularity = null
+    DateTime TimeUpdate,
+    int CountUpdatedProperty
 );

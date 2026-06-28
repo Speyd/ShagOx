4f7916fb09a0a40d3182417daa861ba46a3fd7ec
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-28 20:14:19 +0200

    feat(role): add RoleUpdateService

--- backend/ShagOxServer/ShagOxServer.Application/DTOs/Roles/Update/RoleUpdateRequest.cs
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Roles/Update/RoleUpdateRequest.cs
@@ -3,6 +3,6 @@ namespace ShagOxServer.Application.DTOs.Roles.Update;
 public sealed record RoleUpdateRequest
 (
     int Id,
-    string? Title = null,
+    string? Name = null,
     string? Description = null
 );
---
+++ backend/ShagOxServer/ShagOxServer.Application/Interfaces/Roles/Update/IRoleUpdateService.cs
@@ -0,0 +1,9 @@
+﻿using ShagOxServer.Application.Common.Results;
+using ShagOxServer.Application.DTOs.Roles.Update;
+
+namespace ShagOxServer.Application.Interfaces.Roles.Update;
+public interface IRoleUpdateService
+{
+    Task<Result<RoleUpdateResponse>> UpdateRoleAsync(
+        RoleUpdateRequest request);
+}

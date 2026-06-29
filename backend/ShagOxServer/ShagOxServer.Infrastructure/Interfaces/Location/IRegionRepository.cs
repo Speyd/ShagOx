4144efffeea654112f98d0842906c96307d3de4c
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-29 12:41:32 +0200

    feat(region): add RegionRepository

---
+++ backend/ShagOxServer/ShagOxServer.Infrastructure/Interfaces/Location/IRegionRepository.cs
@@ -0,0 +1,19 @@
+﻿using ShagOxServer.Domain.Entities.Location;
+
+namespace ShagOxServer.Infrastructure.Interfaces.Location;
+internal interface IRegionRepository
+{
+    Task<Region?> GetByIdAsync(int id);
+
+    Task<Region?> GetByNameAsync(string name);
+
+
+    Task<bool> ExistsAsync(string name);
+
+
+    Task AddAsync(Region region);
+
+    Task<bool> UpdateAsync(Region region);
+
+    Task DeleteAsync(Region region);
+}
---
+++ backend/ShagOxServer/ShagOxServer.Infrastructure/Persistence/Repositories/Location/RegionRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShagOxServer.Domain.Entities.Location;
+using ShagOxServer.Infrastructure.Interfaces.Location;
+
+namespace ShagOxServer.Infrastructure.Persistence.Repositories.Location;
+public class RegionRepository : BaseRepository, IRegionRepository
+{
+    public RegionRepository(AppDbContext db)
+        : base(db)
+    { }
+
+    public async Task AddAsync(Region region)
+    {
+        await _db.Regions.AddAsync(region);
+
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task<bool> UpdateAsync(Region region)
+    {
+        _db.Regions.Update(region);
+
+        await _db.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task DeleteAsync(Region region)
+    {
+        _db.Regions.Remove(region);
+
+        await _db.SaveChangesAsync();
+    }
+
+
+    public async Task<bool> ExistsAsync(string name)
+    {
+        return await _db.Regions
+          .AnyAsync(x => x.Name == name);
+    }
+
+    public async Task<Region?> GetByIdAsync(int id)
+    {
+        return await _db.Regions
+            .FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<Region?> GetByNameAsync(string name)
+    {
+        return await _db.Regions
+            .FirstOrDefaultAsync(x => x.Name == name);
+    }
+}

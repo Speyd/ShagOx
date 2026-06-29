d5462642697a9b180e93949a012abce7fe90fbe0
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-29 21:38:53 +0200

    feat(atributte): add AttributeDefinition update request ans response

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Dictionaries/AttributeDefinitions/Update/AttributeDefinitionUpdateRequest.cs
@@ -0,0 +1,12 @@
+﻿using ShagOxServer.Domain.Entities.Dictionaries.Enum;
+
+namespace ShagOxServer.Application.DTOs.Dictionaries.AttributeDefinitions.Update;
+public sealed record AttributeDefinitionUpdateRequest
+(
+    int? CategoryId,
+    string? Key,
+    AttributeType? Type,
+    bool? Required,
+    int? Min,
+    int? Max
+);
---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Dictionaries/AttributeDefinitions/Update/AttributeDefinitionUpdateResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShagOxServer.Application.DTOs.Dictionaries.AttributeDefinitions.Update;
+public sealed record AttributeDefinitionUpdateResponse
+(
+    int Id,
+    DateTime CreatedAt
+);

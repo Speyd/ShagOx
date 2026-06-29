f4bf3da8e70ea36c6d5366e3c7e8d0bcd020e778
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-29 23:54:12 +0200

    feat(category): add CategoryUpdateService

---
+++ backend/ShagOxServer/ShagOxServer.Application/Interfaces/Dictionaries/Categories/Update/ICategoryUpdateService.cs
@@ -0,0 +1,10 @@
+﻿using ShagOxServer.Application.Common.Results;
+using ShagOxServer.Application.DTOs.Dictionaries.Categories.Update;
+
+namespace ShagOxServer.Application.Interfaces.Dictionaries.Categories.Update;
+public interface ICategoryUpdateService
+{
+    Task<Result<CategoryUpdateResponse>> UpdateCategoryAsync(
+        int categoryId,
+        CategoryUpdateRequest request);
+}
---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Dictionaries/Categories/Update/CategoryUpdateService.cs
@@ -0,0 +1,112 @@
+﻿using ShagOxServer.Application.Common.Results;
+using ShagOxServer.Application.DTOs.Dictionaries.Categories.Update;
+using ShagOxServer.Application.Interfaces.Dictionaries.Categories.Update;
+using ShagOxServer.Domain.Entities;
+using ShagOxServer.Domain.Entities.Dictionaries;
+using ShagOxServer.Infrastructure.Interfaces.Advertisements;
+using ShagOxServer.Infrastructure.Interfaces.Dictionaries;
+
+namespace ShagOxServer.Application.Services.Dictionaries.Categories.Update;
+public class CategoryUpdateService : ICategoryUpdateService
+{
+    private readonly ICategoryRepository _categoryRepository;
+    private readonly IAttributeDefinitionRepository _attributeRepository;
+    private readonly IAdvertisementRepository _advertisementRepository;
+
+
+    public CategoryUpdateService(
+        ICategoryRepository categoryRepository,
+        IAttributeDefinitionRepository attributeRepository,
+        IAdvertisementRepository advertisementRepository)
+    {
+        _categoryRepository = categoryRepository;
+        _attributeRepository = attributeRepository;
+        _advertisementRepository = advertisementRepository;
+    }
+
+    public async Task<Result<CategoryUpdateResponse>> UpdateCategoryAsync(
+        int categoryId,
+        CategoryUpdateRequest request)
+    {
+        var category = await _categoryRepository.GetByIdAsync(categoryId);
+        if(category is null)
+            return Result<CategoryUpdateResponse>.NotFound("Category");
+
+
+        var attributes = new List<AttributeDefinition>();
+        if (ValidateAttributes(category, request))
+            attributes = await _attributeRepository.GetByIdsAsync(request.Attributes);
+
+        var advertisements = new List<Advertisement>();
+        if (ValidateAdvertisements(category, request))
+            advertisements = await _advertisementRepository.GetByIdsAsync(request.Advertisements);
+    
+        var updatedCount = ApplyUpdates(category, attributes, advertisements, request);
+        var result = new CategoryUpdateResponse(
+                DateTime.UtcNow,
+                updatedCount
+            );
+
+        if (updatedCount == 0)
+            return Result<CategoryUpdateResponse>.Success(result);
+
+        await _attributeRepository.UpdateAsync(category);
+
+        return Result<CategoryUpdateResponse>.Success(result);
+    }
+
+    private static int ApplyUpdates(
+        Category category,
+        List<AttributeDefinition> Attributes,
+        List<Advertisement> Advertisements,
+        CategoryUpdateRequest request)
+    {
+        int countUpdated = 0;
+
+        if (request.Name is not null)
+        {
+            category.Name = request.Name;
+            countUpdated++;
+        }
+
+        if (request.ProductType is not null)
+        {
+            category.ProductType = request.ProductType.Value;
+            countUpdated++;
+        }
+
+        if (Attributes is not null &&
+            !Attributes.Any())
+        {
+            category.Attributes = Attributes;
+            countUpdated++;
+        }
+
+        if (Advertisements is not null &&
+            !Advertisements.Any())
+        {
+            category.Advertisements = Advertisements;
+            countUpdated++;
+        }
+
+        return countUpdated;
+    }
+
+    private bool ValidateAttributes(
+        Category category,
+        CategoryUpdateRequest request)
+    {
+        return !request.Attributes
+            .Except(category.Attributes.Select(x => x.Id))
+            .Any();
+    }
+
+    private bool ValidateAdvertisements(
+        Category category,
+        CategoryUpdateRequest request)
+    {
+        return !request.Advertisements
+            .Except(category.Advertisements.Select(x => x.Id))
+            .Any();
+    }
+}

1e1896849aba4c38987fd86fe18513c64c5ca354
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-29 21:53:27 +0200

    feat(category): add CategoryDto

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Dictionaries/Categories/CategoryDto.cs
@@ -0,0 +1,11 @@
+﻿using ShagOxServer.Domain.Entities.Dictionaries.Enum;
+
+namespace ShagOxServer.Application.DTOs.Dictionaries.Categories;
+
+public sealed record CategoryDto
+(
+    string Name,
+    ProductType ProductType,
+    List<int> Attributes,
+    List<int> Advertisements
+);

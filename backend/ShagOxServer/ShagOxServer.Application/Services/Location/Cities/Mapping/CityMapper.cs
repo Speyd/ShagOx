13b60f73faa51c6bcf5f3355dcc8cc960c184bc2
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-29 15:19:06 +0200

    feat(city): add CityQueryService

---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Location/Cities/Mapping/CityMapper.cs
@@ -0,0 +1,15 @@
+﻿using ShagOxServer.Application.DTOs.Location.Cities;
+using ShagOxServer.Domain.Entities.Location;
+
+namespace ShagOxServer.Application.Services.Location.Cities.Mapping;
+public static class CityMapper
+{
+    public static CityDto ToDto(City city)
+    {
+        return new CityDto(
+            city.Name,
+            city.RegionId,
+            city.Region?.Name ?? "Unknown region name"
+        );
+    }
+}

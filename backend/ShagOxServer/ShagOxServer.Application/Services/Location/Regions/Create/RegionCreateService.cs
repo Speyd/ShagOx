5d53298b5b8157fca45cae23a7a03a3fd23c29c6
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-29 13:31:20 +0200

    feat(region): add RegionCreateService

---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Location/Regions/Create/RegionCreateService.cs
@@ -0,0 +1,46 @@
+﻿using ShagOxServer.Application.Common.Results;
+using ShagOxServer.Application.DTOs.Advertisements.Create;
+using ShagOxServer.Application.DTOs.Location.Regions.Create;
+using ShagOxServer.Application.Interfaces.Location.Regions.Create;
+using ShagOxServer.Domain.Entities.Location;
+using ShagOxServer.Infrastructure.Interfaces.Location.Regions;
+
+namespace ShagOxServer.Application.Services.Location.Regions.Create;
+public class RegionCreateService : IRegionCreateService
+{
+    private readonly IRegionRepository _repository;
+
+    public RegionCreateService(
+        IRegionRepository regionRepository)
+    {
+        _repository = regionRepository;
+    }
+
+    public async Task<Result<RegionCreateResponse>> CreateRegionAsync(
+        RegionCreateRequest request)
+    {
+        var validation = await _repository.ExistsAsync(request.Name);
+        if (validation)
+            return Result<RegionCreateResponse>.Fail("A region with that name has already been created");
+
+        var region = CreateRegion(request);
+
+        await _repository.AddAsync(region);
+
+        var response = new AdvertisementCreateResponse(
+            region.Id,
+            DateTime.UtcNow
+        );
+
+        return Result<AdvertisementCreateResponse>.Success(response);
+    }
+
+    private Region CreateRegion(
+        RegionCreateRequest request)
+    {
+        return new Region
+        {
+            Name = request.Name
+        };
+    }
+}

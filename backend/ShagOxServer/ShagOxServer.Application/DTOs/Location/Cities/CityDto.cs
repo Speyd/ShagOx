daa86cf59027fc7900521d541bdf56b9082b3b12
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-29 14:52:39 +0200

    feat(city): add CityDto

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Location/Cities/CityDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShagOxServer.Application.DTOs.Location.Cities;
+public sealed record CityDto
+(
+    string Name,
+    int RegionId,
+    string NameRegion
+);

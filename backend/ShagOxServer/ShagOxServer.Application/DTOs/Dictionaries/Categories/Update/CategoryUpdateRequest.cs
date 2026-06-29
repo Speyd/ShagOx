d356c7de1223f79c459e3c7b6f30435181e2f240
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-29 23:33:18 +0200

    feat(category): add Category update response and request

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Dictionaries/Categories/Update/CategoryUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿using ShagOxServer.Domain.Entities.Dictionaries.Enum;
+
+namespace ShagOxServer.Application.DTOs.Dictionaries.Categories.Update;
+public sealed record CategoryUpdateRequest
+(
+    string? Name,
+    ProductType? ProductType,
+    List<int> Attributes,
+    List<int> Advertisements
+);

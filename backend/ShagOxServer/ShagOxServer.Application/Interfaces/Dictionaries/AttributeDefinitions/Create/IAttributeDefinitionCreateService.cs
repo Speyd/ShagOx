f35fbabeddf0400426266cf8d434c51389da7b61
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-29 21:36:10 +0200

    feat(attribute): add AttributeDefinitionCreateService

---
+++ backend/ShagOxServer/ShagOxServer.Application/Interfaces/Dictionaries/AttributeDefinitions/Create/IAttributeDefinitionCreateService.cs
@@ -0,0 +1,9 @@
+﻿using ShagOxServer.Application.Common.Results;
+using ShagOxServer.Application.DTOs.Dictionaries.AttributeDefinitions.Create;
+
+namespace ShagOxServer.Application.Interfaces.Dictionaries.AttributeDefinitions.Create;
+public interface IAttributeDefinitionCreateService
+{
+    Task<Result<AttributeDefinitionCreateResponse>> CreateAttributeDefinitionAsync(
+        AttributeDefinitionCreateRequest request);
+}
---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Dictionaries/AttributeDefinitions/Create/AttributeDefinitionCreateService.cs
@@ -0,0 +1,47 @@
+﻿using ShagOxServer.Application.Common.Results;
+using ShagOxServer.Application.DTOs.Dictionaries.AttributeDefinitions.Create;
+using ShagOxServer.Application.Interfaces.Dictionaries.AttributeDefinitions.Create;
+using ShagOxServer.Domain.Entities.Dictionaries;
+using ShagOxServer.Infrastructure.Interfaces.Dictionaries;
+
+namespace ShagOxServer.Application.Services.Dictionaries.AttributeDefinitions.Create;
+public class AttributeDefinitionCreateService : IAttributeDefinitionCreateService
+{
+    private readonly IAttributeDefinitionRepository _repository;
+
+    public AttributeDefinitionCreateService(
+        IAttributeDefinitionRepository attributeRepository)
+    {
+        _repository = attributeRepository;
+    }
+
+    public async Task<Result<AttributeDefinitionCreateResponse>> CreateAttributeDefinitionAsync(
+        AttributeDefinitionCreateRequest request)
+    {
+        var attribute = CreateAttributeDefinition(request);
+
+        await _repository.AddAsync(attribute);
+
+        var response = new AttributeDefinitionCreateResponse(
+            attribute.Id,
+            DateTime.UtcNow
+        );
+
+        return Result<AttributeDefinitionCreateResponse>.Success(response);
+    }
+
+    private AttributeDefinition CreateAttributeDefinition(
+        AttributeDefinitionCreateRequest request)
+    {
+        return new AttributeDefinition
+        {
+            CategoryId = request.CategoryId,
+            Key = request.Key,
+            Type = request.Type,
+            Required = request.Required,
+            Min = request.Min,
+            Max = request.Max,
+        };
+    }
+
+}

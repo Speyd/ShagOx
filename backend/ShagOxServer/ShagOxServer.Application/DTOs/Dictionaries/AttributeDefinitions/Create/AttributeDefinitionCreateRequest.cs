803f21afe357c23093732b60c2846d6922f9a994
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-29 21:29:46 +0200

    feat(attribute): add AttributeDefinition create responce and request

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Dictionaries/AttributeDefinitions/Create/AttributeDefinitionCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿using ShagOxServer.Domain.Entities.Dictionaries.Enum;
+
+namespace ShagOxServer.Application.DTOs.Dictionaries.AttributeDefinitions.Create;
+public sealed record AttributeDefinitionCreateRequest
+(
+    int CategoryId,
+    string Key,
+    AttributeType Type,
+    bool Required,
+    int? Min,
+    int? Max
+);

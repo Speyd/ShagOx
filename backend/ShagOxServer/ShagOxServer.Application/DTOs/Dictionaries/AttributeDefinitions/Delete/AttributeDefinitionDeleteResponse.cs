1252010d6d5a3403e589589cd2f052982ef9edd8
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-29 21:47:31 +0200

    feat(attribute): add AttributeDefinitionDeleteResponse

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Dictionaries/AttributeDefinitions/Delete/AttributeDefinitionDeleteResponse.cs
@@ -0,0 +1,7 @@
+﻿
+namespace ShagOxServer.Application.DTOs.Dictionaries.AttributeDefinitions.Delete;
+public sealed record AttributeDefinitionDeleteResponse
+(
+    int Id,
+    DateTime DeleteTime
+);

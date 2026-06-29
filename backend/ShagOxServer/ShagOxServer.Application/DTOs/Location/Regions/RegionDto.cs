f6b0c8e2d0a17da3fa2f4e61ea0537c716fda36e
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-29 12:47:10 +0200

    feat(region): add RegionDto

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Location/Regions/RegionDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShagOxServer.Application.DTOs.Location.Regions;
+
+public sealed record RegionDto
+(
+    string Name
+);

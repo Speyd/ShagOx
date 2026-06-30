740546b47a62787b4b08ee138f9871f909ccd3f4
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-30 13:59:53 +0200

    feat(currency): add Currency update request and response

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Specification/Currencies/Update/CurrencyUpdateResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShagOxServer.Application.DTOs.Specification.Currencies.Update;
+public sealed record CurrencyUpdateResponse
+(
+    DateTime TimeUpdate,
+    int CountUpdatedProperty
+);

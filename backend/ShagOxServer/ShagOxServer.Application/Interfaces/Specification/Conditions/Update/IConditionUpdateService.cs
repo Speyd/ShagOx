fa0e9209fa1145bb765fd23a4bd4a2939db73bc2
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-01 01:09:30 +0200

    feat(condition): add ConditionUpdateService

---
+++ backend/ShagOxServer/ShagOxServer.Application/Interfaces/Specification/Conditions/Update/IConditionUpdateService.cs
@@ -0,0 +1,10 @@
+﻿using ShagOxServer.Application.Common.Results;
+using ShagOxServer.Application.DTOs.Specification.Conditions.Update;
+
+namespace ShagOxServer.Application.Interfaces.Specification.Conditions.Update;
+public interface IConditionUpdateService
+{
+    Task<Result<ConditionUpdateResponse>> UpdateConditionAsync(
+        int conditionId,
+        ConditionUpdateRequest request);
+}

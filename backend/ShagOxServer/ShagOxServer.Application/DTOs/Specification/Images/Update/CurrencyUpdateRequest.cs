4d571241b30e9e89526115b7fec33e7cb4d59f1c
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-01 01:43:03 +0200

    feat(image): add Image update request and response

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Specification/Images/Update/CurrencyUpdateRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace ShagOxServer.Application.DTOs.Specification.Images.Update;
+public sealed record  CurrencyUpdateRequest
+(
+    string? Url,
+    int? Order,
+    int? AdvertisementId
+);
---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Specification/Images/Update/CurrencyUpdateResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace ShagOxServer.Application.DTOs.Specification.Images.Update;
+public sealed record CurrencyUpdateResponse
+(
+    DateTime TimeUpdate,
+    int CountUpdatedProperty
+);

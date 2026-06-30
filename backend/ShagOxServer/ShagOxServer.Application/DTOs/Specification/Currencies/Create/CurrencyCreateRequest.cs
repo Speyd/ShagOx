f3bf9bc95345e33d3e4b1677e81c1045627a9a9b
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-30 13:53:52 +0200

    feat(currency): add Curency create request and response

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Specification/Currencies/Create/CurrencyCreateRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShagOxServer.Application.DTOs.Specification.Currencies.Create;
+public sealed record CurrencyCreateRequest
+(
+    string Code,
+    string Symbol,
+    string Name
+);
---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Specification/Currencies/Create/CurrencyCreateResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShagOxServer.Application.DTOs.Specification.Currencies.Create;
+public sealed record CurrencyCreateResponse
+(
+    int Id,
+    DateTime CreatedAt
+);

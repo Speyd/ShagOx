0100b75727dc6b882a40c4a3d5d735bcc499eaf7
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-06-30 13:51:47 +0200

    feat(currency): add CurrencyQueryService

---
+++ backend/ShagOxServer/ShagOxServer.Application/Interfaces/Specification/Currencies/Query/ICurrencyQueryService.cs
@@ -0,0 +1,13 @@
+﻿using ShagOxServer.Application.Common.Results;
+using ShagOxServer.Application.DTOs.Specification.Currencies;
+
+
+namespace ShagOxServer.Application.Interfaces.Specification.Currencies.Query;
+public interface ICurrencyQueryService
+{
+    Task<Result<CurrencyDto>> GetByIdAsync(int id);
+
+    Task<Result<CurrencyDto>> GetByCodeAsync(string code);
+
+    Task<Result<CurrencyDto>> GetBySymbolAsync(string symbol);
+}
---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Currencies/Mapping/CurrencyMapper.cs
@@ -0,0 +1,17 @@
+﻿using ShagOxServer.Application.DTOs.Specification.Currencies;
+using ShagOxServer.Domain.Entities.Specification;
+
+namespace ShagOxServer.Application.Services.Currencies.Mapping;
+public static class CurrencyMapper
+{
+    public static CurrencyDto ToDto(Currency x)
+    {
+        return new CurrencyDto
+        (
+            x.Id,
+            x.Code,
+            x.Symbol,
+            x.Name
+        );
+    }
+}

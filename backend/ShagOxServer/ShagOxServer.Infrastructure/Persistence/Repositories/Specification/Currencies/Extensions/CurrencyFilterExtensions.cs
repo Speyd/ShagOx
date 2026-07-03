4ab2138554c1b1753072b19c9da8afcd42b2c172
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-03 11:49:41 +0200

    feat(currency): add CurrencyFilterExtensions

---
+++ backend/ShagOxServer/ShagOxServer.Infrastructure/Persistence/Repositories/Specification/Currencies/Extensions/CurrencyFilterExtensions.cs
@@ -0,0 +1,31 @@
+﻿using ShagOxServer.Domain.Entities.Specification;
+using ShagOxServer.Domain.Filters.Specification.Currencies;
+
+namespace ShagOxServer.Infrastructure.Persistence.Repositories.Specification.Currencies.Extensions;
+public static class CurrencyFilterExtensions
+{
+    public static IQueryable<Currency> Filter(
+       this IQueryable<Currency> query,
+       CurrencySearchFilter filter)
+    {
+        if (filter is null)
+            return query;
+
+        if (!string.IsNullOrWhiteSpace(filter.Code))
+        {
+            query = query.Where(u => u.Code.Contains(filter.Code));
+        }
+
+        if (!string.IsNullOrWhiteSpace(filter.Symbol))
+        {
+            query = query.Where(u => u.Symbol.Contains(filter.Symbol));
+        }
+
+        if (!string.IsNullOrWhiteSpace(filter.Name))
+        {
+            query = query.Where(u => u.Name.Contains(filter.Name));
+        }
+
+        return query;
+    }
+}

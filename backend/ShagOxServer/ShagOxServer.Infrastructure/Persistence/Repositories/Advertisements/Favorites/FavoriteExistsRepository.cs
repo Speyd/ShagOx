ea90badc5d61367c71c034df9aea443beda8a18c
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-07 18:51:36 +0200

    feat(favorite): add FavoriteExistsRepository

---
+++ backend/ShagOxServer/ShagOxServer.Infrastructure/Persistence/Repositories/Advertisements/Favorites/FavoriteExistsRepository.cs
@@ -0,0 +1,9 @@
+﻿using ShagOxServer.Infrastructure.Interfaces.Advertisements.Favorites;
+
+namespace ShagOxServer.Infrastructure.Persistence.Repositories.Advertisements.Favorites;
+public class FavoriteExistsRepository : BaseRepository, IFavoriteExistsRepository
+{
+    public FavoriteExistsRepository(AppDbContext db)
+        : base(db)
+    { }
+}

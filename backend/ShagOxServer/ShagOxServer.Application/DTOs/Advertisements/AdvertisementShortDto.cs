426a5aa07332cb31a00e068f3ee9c7f99cdea1ac
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-07 19:23:18 +0200

    feat(favorite): add FavoriteDto

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Advertisements/AdvertisementShortDto.cs
@@ -0,0 +1,17 @@
+﻿namespace ShagOxServer.Application.DTOs.Advertisements;
+public sealed record AdvertisementShortDto
+(
+    int Id,
+    string Title,
+    string Description,
+    int Price,
+    int PreviousPrice,
+    int CurrencyId,
+    int CategoryId,
+    int SellerId,
+    int? BuyerId,
+    List<int> ImageIds,
+    Dictionary<string, string> Properties,
+    DateTime? SoldAt,
+    DateTime CreatedAt
+);
---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Advertisements/Favorites/FavoriteDto.cs
@@ -0,0 +1,8 @@
+﻿using ShagOxServer.Application.DTOs.Users;
+
+namespace ShagOxServer.Application.DTOs.Advertisements.Favorites;
+public sealed record FavoriteDto
+(
+    UserShortDto User,
+    AdvertisementShortDto Advertisement
+);

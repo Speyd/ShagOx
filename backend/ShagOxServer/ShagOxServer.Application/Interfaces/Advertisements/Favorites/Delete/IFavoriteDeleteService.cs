86fb4ab23631f176d259f5649877801055d7fd05
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-08 11:49:16 +0200

    feat(favorite): add FavoriteDeleteService

---
+++ backend/ShagOxServer/ShagOxServer.Application/Interfaces/Advertisements/Favorites/Delete/IFavoriteDeleteService.cs
@@ -0,0 +1,9 @@
+﻿using ShagOxServer.Application.DTOs.Advertisements.Favorites.Delete;
+using ShagOxServer.SharedKernel.Abstractions.Results;
+
+namespace ShagOxServer.Application.Interfaces.Advertisements.Favorites.Delete;
+public interface IFavoriteDeleteService
+{
+    Task<Result<FavoriteDeleteResponse>> DeleteFavoriteAsync(
+       int id);
+}
---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Advertisements/Favorites/Delete/FavoriteDeleteService.cs
@@ -0,0 +1,33 @@
+﻿using ShagOxServer.Application.DTOs.Advertisements.Favorites.Delete;
+using ShagOxServer.Application.Interfaces.Advertisements.Favorites.Delete;
+using ShagOxServer.Infrastructure.Interfaces.Advertisements.Favorites;
+using ShagOxServer.SharedKernel.Abstractions.Results;
+
+namespace ShagOxServer.Application.Services.Advertisements.Favorites.Delete;
+public class FavoriteDeleteService : IFavoriteDeleteService
+{
+    private readonly IFavoriteRepository _repository;
+
+
+    public FavoriteDeleteService(
+        IFavoriteRepository favoriteRepository)
+    {
+        _repository = favoriteRepository;
+    }
+
+    public async Task<Result<FavoriteDeleteResponse>> DeleteFavoriteAsync(
+        int id)
+    {
+        var favorite = await _repository.GetByIdAsync(id);
+        if (favorite is null)
+            return Result<FavoriteDeleteResponse>.NotFound("Favorite");
+
+        await _repository.DeleteAsync(favorite);
+        return Result<FavoriteDeleteResponse>.Success(
+           new FavoriteDeleteResponse(
+               favorite.Id,
+               DateTime.UtcNow
+           )
+       );
+    }
+}

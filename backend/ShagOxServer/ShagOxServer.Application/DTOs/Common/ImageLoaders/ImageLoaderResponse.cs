bda01514dfedb7583e68fa184148533c40348259
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-08 23:16:57 +0200

    feat(image): add ImageLoaderResponse

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Common/ImageLoaders/ImageLoaderResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace ShagOxServer.Application.DTOs.Common.ImageLoaders;
+public sealed record ImageLoaderResponse
+(
+    int PublicId,
+    string Url
+);
---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Common/ImageLoaders/CloudinaryService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShagOxServer.Application.Services.Common.ImageLoaders
+{
+    internal class CloudinaryService
+    {
+    }
+}

87a2c2147bf00f9c51ac01ddd0b04cb2fb4d6cf8
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-08 22:34:41 +0200

    feat(favorite): add Favorite to Dependencyinjection

--- backend/ShagOxServer/ShagOxServer.Application/DependencyInjections/AdvertisementDependencyInjection.cs
+++ backend/ShagOxServer/ShagOxServer.Application/DependencyInjections/AdvertisementDependencyInjection.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using ShagOxServer.Application.Interfaces.Advertisements.Create;
 using ShagOxServer.Application.Interfaces.Advertisements.Delete;
+using ShagOxServer.Application.Interfaces.Advertisements.Favorites.Create;
+using ShagOxServer.Application.Interfaces.Advertisements.Favorites.Delete;
+using ShagOxServer.Application.Interfaces.Advertisements.Favorites.Query;
+using ShagOxServer.Application.Interfaces.Advertisements.Favorites.Update;
 using ShagOxServer.Application.Interfaces.Advertisements.Query;
 using ShagOxServer.Application.Interfaces.Advertisements.Update;
 using ShagOxServer.Application.Services.Advertisements.Create;
 using ShagOxServer.Application.Services.Advertisements.Delete;
+using ShagOxServer.Application.Services.Advertisements.Favorites.Create;
+using ShagOxServer.Application.Services.Advertisements.Favorites.Delete;
+using ShagOxServer.Application.Services.Advertisements.Favorites.Query;
+using ShagOxServer.Application.Services.Advertisements.Favorites.Update;
 using ShagOxServer.Application.Services.Advertisements.Query;
 using ShagOxServer.Application.Services.Advertisements.Update;
 
@@ -17,6 +25,11 @@ public static class AdvertisementDependencyInjection
         services.AddScoped<IAdvertisementDeleteService, AdvertisementDeleteService>();
         services.AddScoped<IAdvertisementQueryService, AdvertisementQueryService>();
         services.AddScoped<IAdvertisementUpdateService, AdvertisementUpdateService>();
+
+        services.AddScoped<IFavoriteCreateService, FavoriteCreateService>();
+        services.AddScoped<IFavoriteDeleteService, FavoriteDeleteService>();
+        services.AddScoped<IFavoriteQueryService, FavoriteQueryService>();
+        services.AddScoped<IFavoriteUpdateService, FavoriteUpdateService>();
 
         return services;
     }
--- backend/ShagOxServer/ShagOxServer.Infrastructure/DependencyInjections/AdvertisementDependencyInjection.cs
+++ backend/ShagOxServer/ShagOxServer.Infrastructure/DependencyInjections/AdvertisementDependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using ShagOxServer.Infrastructure.Interfaces.Advertisements;
+using ShagOxServer.Infrastructure.Interfaces.Advertisements.Favorites;
 using ShagOxServer.Infrastructure.Persistence.Repositories.Advertisements;
+using ShagOxServer.Infrastructure.Persistence.Repositories.Advertisements.Favorites;
 
 namespace ShagOxServer.Infrastructure.DependencyInjections;
 public static class AdvertisementDependencyInjection
@@ -10,6 +12,10 @@ public static class AdvertisementDependencyInjection
         services.AddScoped<IAdvertisementRepository, AdvertisementRepository>();
         services.AddScoped<IAdvertisementQueryRepository, AdvertisementQueryRepository>();
         services.AddScoped<IAdvertisementExistsRepository, AdvertisementExistsRepository>();
+
+        services.AddScoped<IFavoriteRepository, FavoriteRepository>();
+        services.AddScoped<IFavoriteQueryRepository, FavoriteQueryRepository>();
+        services.AddScoped<IFavoriteExistsRepository, FavoriteExistsRepository>();
 
         return services;
     }

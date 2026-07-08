ace73ea16a1584c5ce5fcecd9fd0b93ab680faf2
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-08 23:23:19 +0200

    feat(image): add ImageLoaderDeleteResponse)

---
+++ backend/ShagOxServer/ShagOxServer.Application/DTOs/Common/ImageLoaders/Delete/ImageLoaderDeleteResponse.cs.cs
@@ -0,0 +1,6 @@
+﻿namespace ShagOxServer.Application.DTOs.Common.ImageLoaders.Delete;
+public sealed record ImageLoaderDeleteResponse
+(
+    int PublicId,
+    DateTime DeleteTime
+);
--- backend/ShagOxServer/ShagOxServer.Application/Services/Common/ImageLoaders/CloudinaryService.cs
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Common/ImageLoaders/CloudinaryService.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using ShagOxServer.Application.Interfaces.Common.ImageLoaders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ShagOxServer.Application.Services.Common.ImageLoaders
+namespace ShagOxServer.Application.Services.Common.ImageLoaders;
+
+public class CloudinaryService : IImageLoader
 {
-    internal class CloudinaryService
+    public Task DeleteAsync(string publicId)
     {
+        throw new NotImplementedException();
+    }
+
+    public Task<(string Url, string PublicId)> UploadAsync(IFormFile file)
+    {
+        throw new NotImplementedException();
     }
 }

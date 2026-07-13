7f16f8f2c73bbffc88a9360e4859885ec90eb1f2
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-14 00:21:35 +0200

    feat(favorite): add FavoriteValidator

---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Advertisements/Favorites/Validator/FavoriteValidator.cs
@@ -0,0 +1,26 @@
+﻿using ShagOxServer.Application.Interfaces.Repositories.Advertisements.Favorites;
+using ShagOxServer.Domain.Entities.Advertisements;
+using ShagOxServer.SharedKernel.Abstractions.Results;
+
+namespace ShagOxServer.Application.Services.Advertisements.Favorites.Validator;
+public class FavoriteValidator
+{
+    private readonly IFavoriteRepository _favoriteRepository;
+
+
+    public FavoriteValidator(
+        IFavoriteRepository favoriteRepository)
+    {
+        _favoriteRepository = favoriteRepository;
+    }
+
+    public async Task<Result<Favorite>> GetFavoriteValidator(
+        int favoriteId)
+    {
+        var favorite = await _favoriteRepository.GetByIdAsync(favoriteId);
+        if (favorite is null)
+            return Result<Favorite>.NotFound("Favorite");
+
+        return Result<Favorite>.Success(favorite);
+    }
+}

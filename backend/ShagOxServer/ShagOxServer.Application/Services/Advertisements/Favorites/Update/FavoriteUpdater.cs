3300d1253a55cb8058c30cf7a1defa6b11c89ed9
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-14 00:39:53 +0200

    refactor(favorite): add FavoriteUpdateService and FavoriteUpdateValidator

---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Advertisements/Favorites/Update/FavoriteUpdater.cs
@@ -0,0 +1,27 @@
+﻿using ShagOxServer.Application.DTOs.Advertisements.Favorites.Update;
+using ShagOxServer.Domain.Entities.Advertisements;
+
+namespace ShagOxServer.Application.Services.Advertisements.Favorites.Update;
+public static class FavoriteUpdater
+{
+    public static int ApplyUpdates(
+        Favorite favorite,
+        FavoriteUpdateRequest request)
+    {
+        int countUpdated = 0;
+
+        if (request.UserId is not null)
+        {
+            favorite.UserId = request.UserId.Value;
+            countUpdated++;
+        }
+
+        if (request.AdvertisementId is not null)
+        {
+            favorite.AdvertisementId = request.AdvertisementId.Value;
+            countUpdated++;
+        }
+
+        return countUpdated;
+    }
+}

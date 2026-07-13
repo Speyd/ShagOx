3ce5bbbc9fe8638687bfa0db813b3e6a8ee59af9
Egor Starostenko <egorstarostenko345@gmail.com>
2026-07-13 22:47:10 +0300

    fix(advertisements): improve delete advertisement hook

--- frontend/src/features/delete-advertisement/hooks/useDeleteAdvertisement.ts
+++ frontend/src/features/delete-advertisement/hooks/useDeleteAdvertisement.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteAdvertisement } from "../model/api";
 import { toast } from "sonner";
+import axios from "axios";
 
 export function useDeleteAdvertisement() {
   const queryClient = useQueryClient();
@@ -11,8 +12,13 @@ export function useDeleteAdvertisement() {
       queryClient.invalidateQueries({ queryKey: ["advertisements"] });
       toast.success("Advertisement deleted!");
     },
-    onError: () => {
-      toast.error("Failed to delete advertisement");
+    onError: (error) => {
+      if (axios.isAxiosError(error) && error.response?.status === 403) {
+        toast.error("Ви не є власником цього оголошення.");
+        return;
+      }
+
+      toast.error("Не вдалося видалити оголошення. Спробуйте ще раз.");
     },
   });
 }

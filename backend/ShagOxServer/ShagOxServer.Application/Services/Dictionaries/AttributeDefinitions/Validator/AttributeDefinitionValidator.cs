0b0338283435e11bffbead95ba4fd27c854d5dad
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-14 22:42:11 +0200

    refactor(role): add AttributeDefinitionValidator

---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Dictionaries/AttributeDefinitions/Validator/AttributeDefinitionValidator.cs
@@ -0,0 +1,40 @@
+﻿using ShagOxServer.Application.Interfaces.Repositories.Dictionaries.AttributeDefinitions;
+using ShagOxServer.Domain.Entities.Dictionaries;
+using ShagOxServer.SharedKernel.Abstractions.Results;
+
+namespace ShagOxServer.Application.Services.Dictionaries.AttributeDefinitions.Validator;
+public class AttributeDefinitionValidator
+{
+    private readonly IAttributeDefinitionRepository _attributeRepository;
+    private readonly IAttributeDefinitionExistsRepository _attributeExistsRepository;
+
+
+    public AttributeDefinitionValidator(
+        IAttributeDefinitionRepository attributeRepository,
+        IAttributeDefinitionExistsRepository attributeExistsRepository)
+    {
+        _attributeRepository = attributeRepository;
+        _attributeExistsRepository = attributeExistsRepository;
+    }
+
+
+    public async Task<Result<AttributeDefinition>> GetAttributeValidator(
+        int attributeId)
+    {
+        var attribute = await _attributeRepository.GetByIdAsync(attributeId);
+        if (attribute is null)
+            return Result<AttributeDefinition>.NotFound("AttributeDefinition");
+
+        return Result<AttributeDefinition>.Success(attribute);
+    }
+
+    public async Task<Result<bool>> ExistsCategoryValidator(
+       int attributeId)
+    {
+        var category = await _attributeExistsRepository.ExistsByIdAsync(attributeId);
+        if (category)
+            return Result<bool>.AlreadyExists("Category");
+
+        return Result<bool>.Success(category);
+    }
+}

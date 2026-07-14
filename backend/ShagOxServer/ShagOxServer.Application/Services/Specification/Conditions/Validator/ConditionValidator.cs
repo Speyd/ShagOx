cbd0e76776dd4e20e693031e4ca5b5cae4aba6a8
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-14 15:45:32 +0200

    refactor(condition): add ConditionValidator

---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Specification/Conditions/Validator/ConditionValidator.cs
@@ -0,0 +1,39 @@
+﻿using ShagOxServer.Application.Interfaces.Repositories.Specification.Conditions;
+using ShagOxServer.Domain.Entities.Specification;
+using ShagOxServer.SharedKernel.Abstractions.Results;
+
+namespace ShagOxServer.Application.Services.Specification.Conditions.Validator;
+public class ConditionValidator
+{
+    private readonly IConditionRepository _repository;
+    private readonly IConditionExistsRepository _existsRepository;
+
+
+    public ConditionValidator(
+        IConditionRepository conditionRepository,
+        IConditionExistsRepository existsRepository)
+    {
+        _repository = conditionRepository;
+        _existsRepository = existsRepository;
+    }
+
+    public async Task<Result<Condition>> GetConditionValidator(
+       int conditionId)
+    {
+        var condition = await _repository.GetByIdAsync(conditionId);
+        if (condition is null)
+            return Result<Condition>.NotFound("Condition");
+
+        return Result<Condition>.Success(condition);
+    }
+
+    public async Task<Result<bool>> ExistsByNameValidator(
+      string name)
+    {
+        var condition = await _existsRepository.ExistsByNameAsync(name);
+        if (!condition)
+            return Result<bool>.NotFound("Condition");
+
+        return Result<bool>.Success(true);
+    }
+}

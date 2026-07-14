3d9bba9c59a2a09627eaab28d9c415ba7b1c85f0
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-14 21:19:44 +0200

    refactor(role): add RegionValidator

---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Location/Regions/Validator/RegionValidator.cs
@@ -0,0 +1,40 @@
+﻿using ShagOxServer.Application.Interfaces.Repositories.Location.Regions;
+using ShagOxServer.Domain.Entities.Location;
+using ShagOxServer.SharedKernel.Abstractions.Results;
+
+namespace ShagOxServer.Application.Services.Location.Regions.Validator;
+public class RegionValidator
+{
+    private readonly IRegionRepository _regionRepository;
+    private readonly IRegionExistsRepository _regionExistsRepository;
+
+
+    public RegionValidator(
+        IRegionRepository regionRepository,
+        IRegionExistsRepository regionExistsRepository)
+    {
+        _regionRepository = regionRepository;
+        _regionExistsRepository = regionExistsRepository;
+    }
+
+
+    public async Task<Result<Region>> GetCityValidator(
+        int regionId)
+    {
+        var region = await _regionRepository.GetByIdAsync(regionId);
+        if (region is null)
+            return Result<Region>.NotFound("Region");
+
+        return Result<Region>.Success(region);
+    }
+
+    public async Task<Result<bool>> ExistsCityValidator(
+       string name)
+    {
+        var region = await _regionExistsRepository.ExistsAsync(name);
+        if (!region)
+            return Result<bool>.AlreadyExists("Region");
+
+        return Result<bool>.Success(region);
+    }
+}

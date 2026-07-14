487ee54d369cde231ae375e754800ddc54d3ab92
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-14 22:52:11 +0200

    refactor(role): add AttributeDefinitionUpdateValidator

---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Dictionaries/AttributeDefinitions/Update/Validator/AttributeDefinitionUpdateValidator.cs
@@ -0,0 +1,23 @@
+﻿using ShagOxServer.Application.DTOs.Dictionaries.AttributeDefinitions.Update;
+using ShagOxServer.Domain.Entities.Dictionaries;
+using ShagOxServer.SharedKernel.Abstractions.Results;
+
+namespace ShagOxServer.Application.Services.Dictionaries.AttributeDefinitions.Update.Validator;
+public class AttributeDefinitionUpdateValidator
+{
+    public Result<(string key, int categoryId)> HasChangesValidator(
+       AttributeDefinition attribute,
+       AttributeDefinitionUpdateRequest request)
+    {
+        var key = request.Key ?? attribute.Key;
+        var categoryId = request.CategoryId ?? attribute.CategoryId;
+
+        if (key == attribute.Key &&
+            categoryId == attribute.CategoryId)
+        {
+            return Result<(string, int)>.Fail("");
+        }
+
+        return Result<(string, int)>.Success((key, categoryId));
+    }
+}

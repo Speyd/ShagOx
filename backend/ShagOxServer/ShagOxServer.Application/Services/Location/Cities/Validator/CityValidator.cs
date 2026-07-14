ca086b46f78401b6ce730232ebf8e790e47369fb
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-14 21:17:25 +0200

    refactor(role): add CityValidator

---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Location/Cities/Validator/CityValidator.cs
@@ -0,0 +1,40 @@
+﻿using ShagOxServer.Application.Interfaces.Repositories.Location.Cities;
+using ShagOxServer.Domain.Entities.Location;
+using ShagOxServer.SharedKernel.Abstractions.Results;
+
+namespace ShagOxServer.Application.Services.Location.Cities.Validator;
+public class CityValidator
+{
+    private readonly ICityRepository _cityRepository;
+    private readonly ICityExistsRepository _cityExistsRepository;
+
+
+    public CityValidator(
+        ICityRepository cityRepository,
+        ICityExistsRepository cityExistsRepository)
+    {
+        _cityRepository = cityRepository;
+        _cityExistsRepository = cityExistsRepository;
+    }
+
+
+    public async Task<Result<City>> GetCityValidator(
+        int cityId)
+    {
+        var city = await _cityRepository.GetByIdAsync(cityId);
+        if (city is null)
+            return Result<City>.NotFound("City");
+
+        return Result<City>.Success(city);
+    }
+
+    public async Task<Result<bool>> ExistsCityValidator(
+       int cityId)
+    {
+        var city = await _cityExistsRepository.ExistsAsync(cityId);
+        if (!city)
+            return Result<bool>.AlreadyExists("City");
+
+        return Result<bool>.Success(city);
+    }
+}

75e56bdab69e2b2b4202e7a01de12510ae60df43
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-07-28 00:49:32 +0200

    feat(page): add class PagedResult

---
+++ backend/ShagOxServer/ShagOxServer.SharedKernel/Abstractions/Paginations/PagedResult.cs
@@ -0,0 +1,27 @@
+﻿namespace ShagOxServer.SharedKernel.Abstractions.Paginations;
+public sealed class PagedResult<T>
+{
+    public IReadOnlyList<T> Items { get; init; } = [];
+
+    public int Page { get; init; }
+
+    public int PageSize { get; init; }
+
+    public int TotalCount { get; init; }
+
+    public int TotalPages { get; init; }
+
+    public PagedResult(
+        IReadOnlyList<T> items, 
+        PaginationParams pagination)
+    {
+        Items = items;
+
+        Page = pagination.Page;
+        PageSize = pagination.PageSize;
+
+        TotalCount = items.Count;
+        TotalPages = (int)Math.Ceiling(
+            items.Count / (double)pagination.PageSize);
+    }
+}

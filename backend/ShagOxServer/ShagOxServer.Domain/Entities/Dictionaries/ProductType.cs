86f87c983c03d7abbeeb13d535a47f96f88190b2
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-08-01 15:50:23 +0200

    feat(productType): add class ProductType

---
+++ backend/ShagOxServer/ShagOxServer.Domain/Entities/Dictionaries/ProductType.cs
@@ -0,0 +1,14 @@
+﻿using ShagOxServer.Domain.Base;
+namespace ShagOxServer.Domain.Entities.Dictionaries;
+
+public class ProductType : BaseEntity
+{
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = "";
+
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}

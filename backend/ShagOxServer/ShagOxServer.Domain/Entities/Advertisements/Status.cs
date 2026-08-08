b82300637f2fee0bf2e2d6860931e889bf337793
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-08-08 11:35:48 +0200

    feat(status): add Advertisement status

---
+++ backend/ShagOxServer/ShagOxServer.Domain/Entities/Advertisements/Status.cs
@@ -0,0 +1,14 @@
+﻿using ShagOxServer.Domain.Base;
+
+namespace ShagOxServer.Domain.Entities.Advertisements;
+public class Status : BaseEntity
+{
+    public string Code { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public override string ToString()
+    {
+        return $"{Code} - {Name}";
+    }
+}

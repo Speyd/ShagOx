997b2b8374841d60596e06feb97b1d2e70d1e3f2
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-08-11 17:16:43 +0200

    feat(status): add Commans and Queries status'es controllers

---
+++ backend/ShagOxServer/ShagOxServer.Api/Controllers/Statuses/StatusQueriesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShagOxServer.Application.Interfaces.Services.Advertisements.Statuses.Query;
+using ShagOxServer.Domain.Filters.Advertisements;
+using ShagOxServer.SharedKernel.Abstractions.Paginations;
+using ShagOxServer.SharedKernel.Abstractions.Results.Extensions;
+
+namespace ShagOxServer.Api.Controllers.Statuses;
+
+[ApiController]
+[Route("api/admin/statuses")]
+[Authorize(Roles = "Admin")]
+public class StatusQueriesController : ApiController
+{
+    private readonly IStatusQueryService _queryService;
+
+
+    public StatusQueriesController(
+        IStatusQueryService queryService)
+    {
+        _queryService = queryService;
+    }
+
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(
+        [FromRoute] int id)
+    {
+        var result = await _queryService
+            .GetByIdAsync(id);
+
+        return result.ToActionResult();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetPaged(
+        [FromQuery] PaginationParams pagination)
+    {
+        var result = await _queryService
+            .GetPagedAsync(pagination);
+
+        return result.ToActionResult();
+    }
+
+    [HttpGet("search")]
+    public async Task<IActionResult> Search(
+        [FromQuery] StatusSearchFilter filter,
+        [FromQuery] PaginationParams pagination)
+    {
+        var result = await _queryService
+            .Search(filter, pagination);
+
+        return result.ToActionResult();
+    }
+}

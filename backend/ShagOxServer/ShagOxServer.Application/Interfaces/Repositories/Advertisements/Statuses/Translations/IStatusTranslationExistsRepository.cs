394887c0b2972065d318d7b12d8821650902ab33
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-08-13 16:11:33 +0200

    feat(status): add StatusTranslationExistsRepository

---
+++ backend/ShagOxServer/ShagOxServer.Application/Interfaces/Repositories/Advertisements/Statuses/Translations/IStatusTranslationExistsRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShagOxServer.Application.Interfaces.Repositories.Base;
+using ShagOxServer.Domain.Localizations.Advertisements;
+
+namespace ShagOxServer.Application.Interfaces.Repositories.Advertisements.Statuses.Translations;
+public interface IStatusTranslationExistsRepository
+     : IExistsRepository<StatusTranslation>
+{
+    Task<bool> ExistsByCodeAsync(
+        string name);
+
+    Task<bool> ExistsByLanguageAsync(
+        string language);
+}
---
+++ backend/ShagOxServer/ShagOxServer.Infrastructure/Persistence/Repositories/Advertisements/Statuses/Translations/StatusTranslationQueryRepository.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShagOxServer.Infrastructure.Persistence.Repositories.Translations.Advertisements.Statuses;
+public class StatusTranslationQueryRepository
+{
+}

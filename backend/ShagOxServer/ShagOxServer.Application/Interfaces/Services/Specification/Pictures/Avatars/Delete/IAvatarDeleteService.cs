f2fc9b303132840cb0551e4467d143efbdac3e04
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-08-20 14:44:22 +0200

    feat(avatar): add AvatarDeleteService

---
+++ backend/ShagOxServer/ShagOxServer.Application/Interfaces/Services/Specification/Pictures/Avatars/Delete/IAvatarDeleteService.cs
@@ -0,0 +1,8 @@
+﻿using ShagOxServer.Application.DTOs.Common.Responses;
+using ShagOxServer.Application.Interfaces.Services.Base;
+
+namespace ShagOxServer.Application.Interfaces.Services.Specification.Pictures.Avatars.Delete;
+public interface IAvatarDeleteService
+    : IDeleteService<DeleteResponse>
+{
+}
---
+++ backend/ShagOxServer/ShagOxServer.Application/Services/Specification/Pictures/Avatars/Delete/AvatarDeleteService.cs
@@ -0,0 +1,58 @@
+﻿using ShagOxServer.Application.DTOs.Common.Responses;
+using ShagOxServer.Application.Interfaces.Persistences;
+using ShagOxServer.Application.Interfaces.Repositories.Base;
+using ShagOxServer.Application.Interfaces.Services.Specification.Pictures.Avatars.Delete;
+using ShagOxServer.Application.Services.Specification.Pictures.Avatars.Validator;
+using ShagOxServer.Domain.Entities.Specification.Pictures;
+using ShagOxServer.SharedKernel.Abstractions.Results;
+
+namespace ShagOxServer.Application.Services.Specification.Pictures.Avatars.Delete;
+public class AvatarDeleteService
+    : IAvatarDeleteService
+{
+    private readonly IRepository<Avatar> _avatarRepository;
+    private readonly AvatarValidator _avatarValidator;
+
+    private readonly IUnitOfWork _unitOfWork;
+
+
+    public AvatarDeleteService(
+        IRepository<Avatar> avatarRepository,
+        AvatarValidator avatarValidator,
+        IUnitOfWork unitOfWork)
+    {
+        _avatarRepository = avatarRepository;
+        _avatarValidator = avatarValidator;
+        _unitOfWork = unitOfWork;
+    }
+
+
+    public async Task<Result<DeleteResponse>> DeleteAsync(
+        int id)
+    {
+        var avatar = await _avatarValidator.GetByIdAsync(id);
+        if (!avatar.IsSuccess)
+            return Result<DeleteResponse>.Fail(avatar.Error);
+
+        await _unitOfWork.BeginTransactionAsync();
+
+        try
+        {
+            _avatarRepository.Add(avatar.Value!);
+
+            await _unitOfWork.CommitAsync();
+        }
+        catch
+        {
+            await _unitOfWork.RollbackAsync();
+            throw;
+        }
+
+        return Result<DeleteResponse>.Success(
+           new DeleteResponse(
+               avatar.Value!.Id,
+               DateTime.UtcNow
+           )
+       );
+    }
+}

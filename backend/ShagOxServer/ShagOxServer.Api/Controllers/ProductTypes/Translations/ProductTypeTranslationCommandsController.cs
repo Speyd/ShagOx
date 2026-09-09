f46770bd9a5e55e4a252c3f4103b01071556bca2
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-09-09 16:30:11 +0200

    feat(productType): add ProductType Translation Controllers

---
+++ backend/ShagOxServer/ShagOxServer.Api/Controllers/ProductTypes/Translations/ProductTypeTranslationCommandsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShagOxServer.Application.DTOs.Dictionaries.ProductTypes.Translations.Create;
+using ShagOxServer.Application.DTOs.Dictionaries.ProductTypes.Translations.Update;
+using ShagOxServer.Application.DTOs.Location.Regions.Translations.Create;
+using ShagOxServer.Application.DTOs.Location.Regions.Translations.Update;
+using ShagOxServer.Application.Interfaces.Services.Dictionaries.ProductTypes.Translations.Create;
+using ShagOxServer.Application.Interfaces.Services.Dictionaries.ProductTypes.Translations.Delete;
+using ShagOxServer.Application.Interfaces.Services.Dictionaries.ProductTypes.Translations.Update;
+using ShagOxServer.SharedKernel.Abstractions.Results.Extensions;
+
+namespace ShagOxServer.Api.Controllers.ProductTypes.Translations;
+
+[ApiController]
+[Route("api/admin/product-types/translations")]
+[Authorize(Roles = "Admin")]
+public class ProductTypeTranslationCommandsController : ApiController
+{
+    private readonly IProductTypeTranslationCreateService _createService;
+    private readonly IProductTypeTranslationUpdateService _updateService;
+    private readonly IProductTypeTranslationDeleteService _deleteService;
+
+
+    public ProductTypeTranslationCommandsController(
+        IProductTypeTranslationCreateService createService,
+        IProductTypeTranslationUpdateService updateService,
+        IProductTypeTranslationDeleteService deleteService)
+    {
+        _createService = createService;
+        _updateService = updateService;
+        _deleteService = deleteService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(
+        ProductTypeTranslationCreateRequest request)
+    {
+        var result = await _createService
+            .CreateAsync(request);
+
+        return result.ToActionResult();
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(
+        [FromRoute] int id,
+        [FromBody] ProductTypeTranslationUpdateRequest request)
+    {
+        var result = await _updateService
+            .UpdateAsync(id, request);
+
+        return result.ToActionResult();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(
+        [FromRoute] int id)
+    {
+        var result = await _deleteService
+            .DeleteAsync(id);
+
+        return result.ToActionResult();
+    }
+}
--- backend/ShagOxServer/ShagOxServer.Api/Controllers/ProductTypes/Translations/ProductTypeTranslationQueriesController.cs
+++ backend/ShagOxServer/ShagOxServer.Api/Controllers/ProductTypes/Translations/ProductTypeTranslationQueriesController.cs
@@ -1,21 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShagOxServer.Application.Interfaces.Providers;
-using ShagOxServer.Application.Interfaces.Services.Location.Regions.Translations.Query;
-using ShagOxServer.Domain.Filters.Location.Regions.Translations;
+using ShagOxServer.Application.Interfaces.Services.Dictionaries.ProductTypes.Translations.Query;
+using ShagOxServer.Domain.Filters.Dictionaries.ProductTypes.Translations;
 using ShagOxServer.SharedKernel.Abstractions.Paginations;
 using ShagOxServer.SharedKernel.Abstractions.Results.Extensions;
 
-namespace ShagOxServer.Api.Controllers.Regions.Translations;
-
+namespace ShagOxServer.Api.Controllers.ProductTypes.Translations;
 [ApiController]
-[Route("api/regions/translations")]
-public class RegionTranslationQueriesController : ApiController
+[Route("api/product-types/translations")]
+public class ProductTypeTranslationQueriesController : ApiController
 {
-    private readonly IRegionTranslationQueryService _queryService;
+    private readonly IProductTypeTranslationQueryService _queryService;
     private readonly ILanguageProvider _languageProvider;
 
-    public RegionTranslationQueriesController(
-        IRegionTranslationQueryService queryService,
+    public ProductTypeTranslationQueriesController(
+        IProductTypeTranslationQueryService queryService,
         ILanguageProvider languageProvider)
     {
         _queryService = queryService;
@@ -48,7 +47,7 @@ public class RegionTranslationQueriesController : ApiController
 
     [HttpGet("search")]
     public async Task<IActionResult> Search(
-        [FromQuery] RegionTranslationSearchFilter filter,
+        [FromQuery] ProductTypeTranslationSearchFilter filter,
         [FromQuery] PaginationParams pagination)
     {
         var result = await _queryService

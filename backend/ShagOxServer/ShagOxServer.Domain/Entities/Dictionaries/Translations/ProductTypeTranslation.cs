8bb6e7368e5bdf01795074be7de2f191fef2a2b9
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-09-09 11:51:21 +0200

    feat(productType): add ProductTypeTranslation

---
+++ backend/ShagOxServer/ShagOxServer.Domain/Entities/Dictionaries/Translations/ProductTypeTranslation.cs
@@ -0,0 +1,13 @@
+﻿using ShagOxServer.Domain.Base;
+
+namespace ShagOxServer.Domain.Entities.Dictionaries.Translations;
+public class ProductTypeTranslation
+    : BaseTranslation<ProductType>
+{
+    public string Name { get; set; } = null!;
+
+    public override string ToString()
+    {
+        return $"{Name}";
+    }
+}
---
+++ backend/ShagOxServer/ShagOxServer.Domain/Filters/Dictionaries/ProductTypes/Translations/ProductTypeTranslationSearchFilter.cs
@@ -0,0 +1,7 @@
+﻿namespace ShagOxServer.Domain.Filters.Dictionaries.ProductTypes.Translations;
+public sealed record ProductTypeTranslationSearchFilter
+(
+    string? ProductTypeCode,
+    string? Language,
+    string? Name
+);

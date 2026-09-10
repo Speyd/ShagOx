5ff909b06bce6eb384cfc64f71441176e7d5ad53
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-09-10 11:44:48 +0200

    feat(category): add CategoryTranslationQueryRepository and CategoryTranslationExistsRepository

---
+++ backend/ShagOxServer/ShagOxServer.Infrastructure/Persistence/Repositories/Dictionaries/Categories/Translations/CategoryTranslationExistsRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShagOxServer.Application.Interfaces.Repositories.Dictionaries.Categories.Translations;
+using ShagOxServer.Domain.Entities.Dictionaries.Translations;
+using ShagOxServer.Infrastructure.Persistence.DbContexts;
+using ShagOxServer.Infrastructure.Persistence.Repositories.Base.Translations;
+
+namespace ShagOxServer.Infrastructure.Persistence.Repositories.Dictionaries.Categories.Translations;
+public class CategoryTranslationExistsRepository
+    : ExistsTranslationRepository<CategoryTranslation>,
+      ICategoryTranslationExistsRepository
+{
+    public CategoryTranslationExistsRepository(AppDbContext db)
+        : base(db)
+    { }
+
+
+    public async Task<bool> ExistsByNameAsync(
+        string name)
+    {
+        return await _db.CategoryTranslations
+            .AnyAsync(x => x.Name == name);
+    }
+}

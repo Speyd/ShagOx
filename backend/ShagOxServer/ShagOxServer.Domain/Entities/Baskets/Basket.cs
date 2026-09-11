b3ff6d254fa5432a117a4656cebaf18df329178b
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-09-11 11:04:09 +0200

    feat(basket): add BasketItem

--- backend/ShagOxServer/ShagOxServer.Domain/Entities/Baskets/Basket.cs
+++ backend/ShagOxServer/ShagOxServer.Domain/Entities/Baskets/Basket.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ShagOxServer.Domain.Entities.Baskets
+namespace ShagOxServer.Domain.Entities.Baskets;
+public class Basket
 {
-    internal class Basket
-    {
-    }
 }
--- backend/ShagOxServer/ShagOxServer.Domain/Entities/Baskets/BasketItem.cs
+++ backend/ShagOxServer/ShagOxServer.Domain/Entities/Baskets/BasketItem.cs
@@ -1,9 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ShagOxServer.Domain.Base;
 
 namespace ShagOxServer.Domain.Entities.Baskets;
 public class BasketItem
-(
+    : BaseEntity
+{
+    public int BasketId { get; set; }
 
-);
+    public int AdvertisementId { get; set; }
+
+    public int Quantity { get; set; }
+
+
+    public override string ToString()
+    {
+        return $"Bask.: {BasketId} | Advert.: {AdvertisementId} | Quant.: {Quantity}";
+    }
+}

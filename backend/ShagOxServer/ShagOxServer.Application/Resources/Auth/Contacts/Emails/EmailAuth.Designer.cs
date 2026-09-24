89770e00e0b41d66855d7a34c21c7f284ce0db11
Nikita Kolesnik <nikitakolesnik.w@gmail.com>
2026-09-24 11:15:45 +0200

    feat(localization): add EmailAuth into entity classes

--- backend/ShagOxServer/ShagOxServer.Application/Resources/Auth/Contacts/Emails/EmailAuth.Designer.cs
+++ backend/ShagOxServer/ShagOxServer.Application/Resources/Auth/Contacts/Emails/EmailAuth.Designer.cs
@@ -22,14 +22,14 @@ namespace ShagOxServer.Application.Resources.Auth.Contacts.Emails {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Email {
+    internal class EmailAuth {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Email() {
+        internal EmailAuth() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace ShagOxServer.Application.Resources.Auth.Contacts.Emails {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ShagOxServer.Application.Resources.Auth.Contacts.Emails.Email", typeof(Email).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ShagOxServer.Application.Resources.Auth.Contacts.Emails.EmailAuth", typeof(EmailAuth).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -61,7 +61,7 @@ namespace ShagOxServer.Application.Resources.Auth.Contacts.Emails {
         }
         
         /// <summary>
-        ///   Ищет локализованную строку, похожую на .
+        ///   Ищет локализованную строку, похожую на Failed to send verification email..
         /// </summary>
         internal static string FailedToSendVerificationEmail {
             get {
@@ -70,7 +70,7 @@ namespace ShagOxServer.Application.Resources.Auth.Contacts.Emails {
         }
         
         /// <summary>
-        ///   Ищет локализованную строку, похожую на .
+        ///   Ищет локализованную строку, похожую на Email is incorrect..
         /// </summary>
         internal static string InvalidEmail {
             get {
